8ce35537f526dda5ff5b86638010cde7b2de6e9d
yaoheling <heling.yao@yahoo.com>
2018-05-23 11:03:05 +0800

    mcipay-merchant

---
+++ merchant-ws/src/main/java/com/mcipay/Bootstrap.java
@@ -0,0 +1,15 @@
+package com.mcipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource(locations={"classpath:application-infrastructure.xml"})
+public class Bootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bootstrap.class, args);
+    }
+
+}
