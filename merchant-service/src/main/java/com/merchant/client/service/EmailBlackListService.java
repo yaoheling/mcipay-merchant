6291fd19b7325f237bb0b1f8c8ef0cf77b30dad8
lvhongye <ufowwe@163.com>
2018-06-08 21:06:53 +0800

    添加邮件黑名单服务

--- merchant-service/src/main/java/com/merchant/client/service/EmailBlackListService.java
+++ merchant-service/src/main/java/com/merchant/client/service/EmailBlackListService.java
@@ -4,6 +4,7 @@ import com.mcipay.persistence.entity.EmailBlackList;
 import com.mcipay.persistence.entity.EmailBlackListKey;
 import com.mcipay.persistence.mapper.EmailBlackListMapper;
 import com.merchant.exception.BusinessException;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -26,6 +27,9 @@ public class EmailBlackListService {
     }
 
     public int save(String email) {
+        if (StringUtils.isBlank(email)) {
+            throw new BusinessException("邮箱不能为空");
+        }
         if (get(email) != null) {
             throw new BusinessException("当前邮件已存在");
         }
@@ -36,6 +40,22 @@ public class EmailBlackListService {
         emailBlackList.setUpdateTime(date);
         emailBlackList.setStatus("1");
         return emailBlackListMapper.insert(emailBlackList);
+    }
+
+    public int update(EmailBlackList bean) {
+        if (bean.getId() == null) {
+            throw new BusinessException("ID不能为空");
+        }
+        if (StringUtils.isBlank(bean.getEmail())) {
+            throw new BusinessException("邮箱不能为空");
+        }
+        bean.setCreateTime(null);
+        bean.setCreateTime(new Date());
+        return emailBlackListMapper.updateByPrimaryKeySelective(bean);
+    }
+
+    public int delete(EmailBlackListKey key) {
+        return emailBlackListMapper.deleteByPrimaryKey(key);
     }
 
 }
