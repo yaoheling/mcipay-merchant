d0a5bb9a61ee4660162801ec89389fa0d3d6c2fe
yaoheling <heling.yao@yahoo.com>
2018-06-19 19:18:25 +0800

    filter

---
+++ merchant-service/src/main/java/com/merchant/CrossOriginFilter.java
@@ -0,0 +1,32 @@
+package com.merchant;
+
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@ServletComponentScan
+@WebFilter(urlPatterns = "/*", filterName = "crossOriginFilter")
+public class CrossOriginFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+}
